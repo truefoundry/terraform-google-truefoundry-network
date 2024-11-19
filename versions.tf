0ba6b7fcdcf03301a73335f03d6279b8b7145ddb
Vedant Pareek <dunefro@gmail.com>
2024-11-19 16:28:16 +0530

    Updating network variables, versions

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.4"
+  required_version = "~> 1.4"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.81.0"
+      version = "~> 6.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.12.1"
+      version = "~> 0.12"
     }
   }
 }
