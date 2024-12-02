5774b3a13a4e2314603d56fc1b50159702dfc5e8
Vedant Pareek <dunefro@gmail.com>
2024-12-02 16:34:00 +0530

    formatted

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "network_id" {
-  value       = var.use_existing_network ? var.network_id  : module.network[0].network_id
+  value       = var.use_existing_network ? var.network_id : module.network[0].network_id
   description = "ID of the network"
 }
 
